a15c628cdf20bfb8232d656f24323466cbd672bb
victoria <victoria97molina@yahoo.com>
2022-08-02 15:35:26 -0500

    Initial Commit

---
+++ Flashzilla/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Flashzilla
+//
+//  Created by Rolando Orta on 8/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Rolando Orta on 8/2/22.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
